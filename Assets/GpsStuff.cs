f6144b3e4a0be1c2b5c2ff773c7636180465e0d9
Alexander Scheurer <mail@aspepex.net>
2016-12-01 16:01:11 +0100

    Fill

---
+++ Assets/GpsStuff.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GpsStuff : MonoBehaviour
+{
+    public Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        Input.location.Start(1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = Input.location.lastData.latitude + " " + Input.location.lastData.longitude;
+    }
+}
